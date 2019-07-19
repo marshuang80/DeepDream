bf9271396531517a3b7a5bb0e0f63dd0c04da09d
marshuang80 <marshuang80@gmail.com>
2019-07-18 20:48:18 -0700

    working code

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import torch
 import torch.nn as nn 
+from  torch.nn.modules.upsampling import Upsample
 import torchvision.models as models
+import torchvision
 import numpy as np
 import matplotlib.pyplot as plt
 import tqdm
 import argparse
 import utils
 import logger
+import cv2
+import PIL
 from scipy.ndimage.filters import gaussian_filter
+from scipy.ndimage import zoom
+from skimage.transform import rescale
 
 def main(args):
 
@@ -19,55 +25,100 @@ def main(args):
     
     # load img
     img = plt.imread(args.input_img)
+    #img = rescale(img, 1.0/4.0)
     norm = lambda x: (x - x.min(axis=(0,1))) / (x.max(axis=(0,1)) - x.min(axis=(0,1)))
     img = norm(img)
     img = np.transpose(img, (2,0,1))
-    img = torch.tensor(img).unsqueeze(0)
-    img = img.type(torch.FloatTensor)
-    img = img.to(device)
-    img.requires_grad = True 
-
-    # TODO tensorboard
-
-    # cuda 
-    device = torch.device(args.device)
+    #img = torch.tensor(img).unsqueeze(0)
+    #img = img.type(torch.FloatTensor)
+    #img = img.to(device)
+    #img.requires_grad = True 
 
     # load pretrained model
     vgg19 = models.vgg19(pretrained=True).features.eval()
+    #vgg19 = models.inception_v3(pretrained=True, transform_input=True).eval()
 
-    model = utils.build_model(vgg19, optim_layer=15, device=device)
+    model = utils.build_model(vgg19, optim_layer=args.layer, device=device)
     model = model.to(device)
 
     loss_fn = utils.L2Loss()
 
-    input_img = img.clone()
 
-    for epoch in range(args.epoch):
+    original_size = img.shape
+    #jitter = np.random.rand(*original_size) - 0.5 # jitter ~ Uniform(0, 1)
+    #img += jitter
+    #octave_imgs = [utils.process_tensor(img, device)]
+    #img = zoom(img, (1, 1/2, 1/2), order=3)
 
-        output = model.forward(input_img)
-        loss = loss_fn(output)
-        loss.backward()
-        print('-'*80)
-        grad = img.grad.cpu().numpy()
-        print(loss)
-        print('-'*80)
+    octave_imgs = [img]
 
-        sigma = (epoch * 6.0) / args.epoch + 0.5
-        grad_smooth1 = gaussian_filter(grad, sigma=sigma)
-        grad_smooth2 = gaussian_filter(grad, sigma=sigma*2)
-        grad_smooth3 = gaussian_filter(grad, sigma=sigma*0.5)
-        grad = (grad_smooth1 + grad_smooth2 + grad_smooth3)
-        grad = np.abs(grad).mean() * grad
+    for octave_itr in range(args.num_octave):
 
-        grad = torch.Tensor(grad).to(device)
+        zoom_img = zoom(octave_imgs[-1], (1, 1/args.octave_ratio, 1/args.octave_ratio), order=3)
+        octave_imgs.append(zoom_img)
 
-        #input_img += args.lr * grad
-        input_img += args.lr * grad
+    octave_imgs = [utils.process_tensor(octave_img, device) for octave_img in octave_imgs]
+    ori_octave_imgs = [octave_img.clone() for octave_img in octave_imgs]
 
-        input_img.data.clamp_(0,1)
-        logger_tb.update_loss('loss ', loss.item(), epoch)
-        logger_tb.update_image('transformation ', input_img.squeeze().cpu().detach().numpy(), epoch)
+    while len(octave_imgs) > 0: 
+        oct_img = octave_imgs.pop()
+        ori_oct_img = ori_octave_imgs.pop()
+        idx = len(octave_imgs)
+        
+        print(f"Octave {idx}")
+            
+        for epoch in range(args.epoch):
+            model.zero_grad()
+            output = model.forward(oct_img)
+            loss = loss_fn(output)
+            loss.backward()
+            grad = oct_img.grad.cpu().numpy()
 
+            # smoothing
+            sigma = (epoch * 4.0) / args.epoch + 0.5
+            grad_smooth1 = gaussian_filter(grad, sigma=sigma)
+            grad_smooth2 = gaussian_filter(grad, sigma=sigma*2)
+            grad_smooth3 = gaussian_filter(grad, sigma=sigma*0.5)
+            #grad_smooth4 = gaussian_filter(grad, sigma=sigma*3)
+            grad = (grad_smooth1 + grad_smooth2 + grad_smooth3)
+            #grad = np.abs(grad).mean() * grad
+            grad = torch.Tensor(grad).to(device)
+
+            lr = args.lr / np.abs(grad.data.cpu().numpy()).mean()
+            oct_img.data += lr * grad.data
+            oct_img.data.clamp_(0,1)
+            oct_img.grad.data.zero_()
+
+            dream_img = oct_img.squeeze().cpu().detach().numpy().copy()
+            logger_tb.update_loss('loss ', loss.item(), epoch)
+            logger_tb.update_image(f'transformation octave{idx}', dream_img, epoch)
+
+       
+        if len(octave_imgs) == 0:
+            break
+
+        h = octave_imgs[-1].shape[2]
+        w = octave_imgs[-1].shape[3]
+
+        difference = oct_img.data - ori_oct_img.data
+        logger_tb.update_image(f'difference', difference, idx)
+
+        #oct_img = Upsample(size=(h,w), mode='nearest')(oct_img)
+        #octave_imgs[-1].data += oct_img
+        #octave_imgs[-1].data = octave_imgs[-1].data /2
+        difference = Upsample(size=(h,w), mode='nearest')(difference)
+        octave_imgs[-1].data += (difference)
+
+        #difference = Upsample(size=(h,w), mode='nearest')(difference)
+        #input_imgs[idx] += input_img
+
+        #octave_imgs[-1].data += (difference/2)
+        #minimum = octave_imgs[-1].min()
+        #maximum = octave_imgs[-1].max()
+        #octave_imgs[-1].data  =  (octave_imgs[-1] - minimum)/(maximum - minimum)
+
+    #logger_tb.update_image(f'result', dream_img, 0)
+    print("done")
 
 if __name__ == "__main__":
     
@@ -77,7 +128,11 @@ if __name__ == "__main__":
     parser.add_argument('--device', type=str, default="cuda")
     parser.add_argument('--epoch', type=int, default=100)
     parser.add_argument('--experiment_name', type=str, default='test')
-    parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--num_octave', type=int, default=2)
+    parser.add_argument('--octave_ratio', type=float, default=0.5)
+    parser.add_argument('--layer', type=int, default=12)
+    parser.add_argument('--model', type=str, default='vgg')
 
     args = parser.parse_args()
 
--- utils.py
+++ utils.py
@@ -31,16 +31,39 @@ def build_model(pretrained, optim_layer, device):
                                          
     model = nn.Sequential()              
     count = 0                            
-                                         
+    layer_count = 0 
+    flag = False                 
     model.add_module('norm', Normalize(device))
-                                         
+    '''
+    for name, child in pretrained.named_children():
+        for l in child.children():
+            model.add_module('{}'.format(layer_count), l) 
+            layer_count += 1
+            if isinstance(l, nn.Conv2d):     
+                if count == optim_layer:     
+                    print("break")
+                    flag = True
+                    break                    
+                count +=1 
+        if flag:
+            break
+    '''
     for i, l in enumerate(pretrained.children()):
         model.add_module('{}'.format(i), l) 
-                                         
         if isinstance(l, nn.Conv2d):     
             if count == optim_layer:     
+                print("break")
                 break                    
-        count +=1                        
-                                        
+            count +=1                        
+    print(model)
     return model                         
+
+def process_tensor(input_img, device):
+
+    input_img = torch.tensor(input_img).unsqueeze(0)
+    input_img = input_img.type(torch.FloatTensor)   
+    input_img = input_img.to(device)                
+    input_img.requires_grad = True                  
+    
+    return input_img
 
