c4057e20fdf6b45ea428611fef1ffa13167fa7f5
marshuang80 <marshuang80@gmail.com>
2019-07-22 20:09:07 -0700

    add run script

---
+++ run.sh
@@ -0,0 +1,9 @@
+python main.py --input_img ./data/tina.jpeg \
+               --device cuda \
+               --epoch 8 \
+               --lr 1e-2 \
+               --experiment_name test_inception \
+               --num_octave 4\
+               --octave_ratio 1.4 \
+               --layer 13
+
