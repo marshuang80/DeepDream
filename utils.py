9a29ce23c37d50791876d72499fd1e278c79b7c8
rohanpaul <rohanpaul14855@github.com>
2019-07-22 20:18:22 -0700

    cleaned utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,9 @@
-import matplotlib.pyplot as plt 
-import scipy.misc as misc
-import numpy as np
-import torch 
+import torch
 import torch.nn as nn
 
+
 class L2Loss(nn.Module):
+    """Loss function for DeepDream is defined as the L2 norm of a predfined layer"""
 
     def __init__(self):
         super(L2Loss, self).__init__()
@@ -14,56 +13,74 @@ class L2Loss(nn.Module):
 
 
 class Normalize(nn.Module):
+    """Normalize each image with the mean and std of the VGG network
+
+    Images have to be normalized with this mean and std to be compatible
+    with the learned parameters of the pretrained network
+
+    Extends:
+        nn.Module
+    """
 
     def __init__(self, device):
 
-         super(Normalize, self).__init__()
+        super(Normalize, self).__init__()
 
-         # vgg mean and std
-         self.mean = torch.tensor([0.485, 0.456, 0.406]).view(-1, 1, 1).float().to(device)
-         self.std = torch.tensor([0.229, 0.224, 0.225]).view(-1, 1, 1).float().to(device)
+        # vgg mean and std
+        self.mean = torch.tensor([0.485, 0.456, 0.406]).view(-1, 1, 1).float().to(device)
+        self.std = torch.tensor([0.229, 0.224, 0.225]).view(-1, 1, 1).float().to(device)
 
     def forward(self, img):
 
         return (img - self.mean) / self.std
 
+
 def build_model(pretrained, optim_layer, device):
-                                         
-    model = nn.Sequential()              
-    count = 0                            
-    layer_count = 0 
-    flag = False                 
+    """Build the DeepDream model by sequentially adding pretrained model layers
+
+    Parameters
+    ----------
+        pretrained: torchvisions.models
+            pretrained CNN model
+        optim_layer: int
+            index of the CNN layer to optimize for deepdream
+        device: torch.device
+            GPU (with number) or CPU
+    """
+
+    model = nn.Sequential()
+    count = 0
+
+    # normalize image with vgg mean and std
     model.add_module('norm', Normalize(device))
-    '''
-    for name, child in pretrained.named_children():
-        for l in child.children():
-            model.add_module('{}'.format(layer_count), l) 
-            layer_count += 1
-            if isinstance(l, nn.Conv2d):     
-                if count == optim_layer:     
-                    print("break")
-                    flag = True
-                    break                    
-                count +=1 
-        if flag:
-            break
-    '''
+
+    # rebuild model until 'optim_layer'
     for i, l in enumerate(pretrained.children()):
-        model.add_module('{}'.format(i), l) 
-        if isinstance(l, nn.Conv2d):     
-            if count == optim_layer:     
+        model.add_module('{}'.format(i), l)
+
+        # count CNN layers
+        if isinstance(l, nn.Conv2d):
+            if count == optim_layer:
                 print("break")
-                break                    
-            count +=1                        
-    print(model)
-    return model                         
+                break
+            count += 1
+    return model
+
 
 def process_tensor(input_img, device):
+    """convert numpy array to tensor and send to device
+
+    Parameters
+    ----------
+        input_img: np.array
+            numpy array containing image
+        device: torch.device
+            GPU (with number) or CPU
+    """
 
     input_img = torch.tensor(input_img).unsqueeze(0)
-    input_img = input_img.type(torch.FloatTensor)   
-    input_img = input_img.to(device)                
-    input_img.requires_grad = True                  
-    
-    return input_img
+    input_img = input_img.type(torch.FloatTensor)
+    input_img = input_img.to(device)
+    input_img.requires_grad = True
 
+    return input_img
